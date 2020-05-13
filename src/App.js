f0beb9e5e65e01b66d892cb898fca0afe8031912
Anton Polevyy <polevyy.anton@gmail.com>
2020-05-13 20:11:14 +1000

    simplified problem immitation

--- src/App.js
+++ src/App.js
@@ -1,18 +1,62 @@
-import React from 'react';
-import { BrowserRouter, Route } from 'react-router-dom';
-import './App.css';
+import React, { useEffect, useState } from 'react';
 
-import MenuBar from './components/MenuBar';
-import Home from './pages/Home';
-import Page1 from './pages/Page1';
+function Home() {
+  return (
+    <h1 style={{ textAlign: 'center' }}>
+      Home Page
+    </h1>
+  );
+}
+
+function Page1() {
+  const [ processId, setProcessId ] = useState(null);
+
+  const getExternalProcessId = () => {
+    // return Math.floor(Math.random() * 10000) + 1;
+    return 'abc123';
+  }
+
+  const stopExternalProcess= (processId) => {
+    alert(`stopping process ${processId}`);
+  }
+
+  useEffect(() => {
+    // Anything in here is fired on component mount.
+    setProcessId( getExternalProcessId() );
+    return () => {
+      // Anything in here is fired on component unmount ()
+      stopExternalProcess(processId);
+    }
+  }, []);
+
+  return (
+    <div>
+      <h1 style={{ textAlign: 'center' }}>
+        Page With Action
+      </h1>
+      <h2>Executing process id: {processId}</h2>
+    </div>
+  );
+}
 
 function App() {
+  const [ isHomePage, setIsHomePage ] = useState(true);
+
   return (
-    <BrowserRouter>
-      <MenuBar />
-      <Route exact path='/' component={Home} />
-      <Route exact path='/page1' component={Page1} />
-    </BrowserRouter>
+    <div>
+      <button 
+        onClick={() => setIsHomePage( !isHomePage )}
+      >
+        Switch component
+      </button>
+      { isHomePage ? 
+        (
+          <Home />
+        ) : (
+          <Page1 />
+        ) 
+      }
+    </div>
   );
 }
 
--- src/pages/Home.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-export default function Home() {
-    return (
-        <h1 style={{ textAlign: 'center' }}>
-            Home Page
-        </h1>
-    );
-}
